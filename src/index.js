e9b8e79fd3efc0e159b6f19385627adc428082ef
Thomas McInnis <hey@thomasmcinnis.com>
2024-01-16 12:56:50 +1100

    Move storage concerns from taskList scope

--- src/index.js
+++ src/index.js
@@ -1,78 +1,111 @@
 /**
- * A singleton class as interface to tasks in localStorage
+ * A class for constructing Task Objects
  */
-class TaskStore {
+class Task {
+    constructor(formData) {
+        if (!formData || !formData.name)
+            throw new Error('Task name is required');
+
+        const { id, name, category, dueDate, isUrgent, createdDate } = formData;
+
+        this.id = id === undefined ? self.crypto.randomUUID() : id;
+        this.name = name;
+        this.category = category;
+        this.dueDate = dueDate;
+        this.isUrgent = isUrgent === undefined ? false : isUrgent;
+        this.createdDate = createdDate === undefined ? new Date() : createdDate;
+    }
+
+    toggleIsUrgent() {
+        this.isUrgent = !this.isUrgent;
+        TaskList.updateStore(); // don't love this ideally Task objects are unaware of updating the data store
+    }
+
+    deleteSelf() {
+        TaskList.deleteTask(this.id);
+    }
+}
+
+/**
+ * A singleton class providing public interfaces to work with the storage
+ */
+class ListStorage {
     // Name to give the  store in localStorage
     static #key = 'task-list';
 
-    // Initialise taskList with localStorage
-    static #taskList = [...this.#loadStore()];
-
     // Load the local data store
-    static #loadStore() {
+    static loadStore() {
         try {
-            const store = JSON.parse(localStorage.getItem(this.#key));
-            if (store !== null) {
-                return store;
-            }
-            return [];
+            return JSON.parse(localStorage.getItem(this.#key));
         } catch (err) {
             console.error(err.message);
         }
     }
 
     // Update the local data store
-    static #updateStore() {
+    static updateStore() {
         try {
-            localStorage.setItem(this.#key, JSON.stringify(this.#taskList));
+            localStorage.setItem(
+                this.#key,
+                JSON.stringify(TaskList.getTasks())
+            );
         } catch (err) {
             console.error(err.message);
         }
     }
+}
+
+/**
+ * A singleton class providing public interfaces to work with the task list
+ */
+class TaskList {
+    // Initialise taskList with localStorage
+    static #tasks;
+
+    // Private method to call if tasks is undefined to initialise from store
+    static #initialiseTasks() {
+        const store = ListStorage.loadStore();
+
+        if (!store) {
+            this.#tasks = [];
+        } else {
+            this.#tasks = store.map((item) => new Task(item));
+        }
+    }
+
+    // Public method to get tasks
+    static getTasks() {
+        if (!this.#tasks) this.#initialiseTasks();
+
+        return this.#tasks;
+    }
 
     // Public method to add task
     static addTask(task) {
-        this.#taskList.push(task);
+        if (!this.#tasks) this.#initialiseTasks();
 
-        this.#updateStore();
+        this.#tasks.push(task);
 
-        console.table(this.#taskList); // Testing only
+        ListStorage.updateStore();
     }
 
     // Public method to delete task
     static deleteTask(id) {
-        const filteredTasks = this.#taskList.filter((task) => {
+        if (!this.#tasks) this.#initialiseTasks();
+
+        if (this.#tasks.length === 0) return;
+
+        const filteredTasks = this.#tasks.filter((task) => {
             return task.id !== id;
         });
 
-        this.#taskList = filteredTasks;
+        this.#tasks = filteredTasks;
 
-        this.#updateStore();
+        ListStorage.updateStore();
+    }
 
-        console.table(this.#taskList); // Testing only
+    // Public method to trigger updating the storage for changes to tasks
+    static updateStore() {
+        ListStorage.updateStore();
     }
 }
-
-class Task {
-    constructor(formData) {
-        if (!formData || !formData.name)
-            throw new Error('Task name is required');
-
-        const { name, category, dueDate, isUrgent } = formData;
-
-        this.id = self.crypto.randomUUID();
-        this.name = name;
-        this.category = category;
-        this.dueDate = dueDate;
-        this.isUrgent = isUrgent == undefined ? false : isUrgent;
-        this.createdDate = new Date();
-    }
-
-    toggleIsUrgent() {
-        this.isUrgent = !this.isUrgent;
-        return this.isUrgent;
-    }
-}
-
-const task1 = new Task({ name: 'my new task', category: 'shopping' });
-const task2 = new Task({ name: 'another task', category: 'work' });

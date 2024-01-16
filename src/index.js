2b362c8ece34373d51f8970cd96f6c4f20693534
Thomas McInnis <hey@thomasmcinnis.com>
2024-01-16 13:47:38 +1100

    Add category list and object class

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
 /**
+ * A class for constructing Category Objects
+ */
+class Category {
+    constructor({ name, id }) {
+        if (!name) throw new Error('Category name is required');
+
+        this.name = name;
+        this.id === undefined ? self.crypto.randomUUID() : id;
+    }
+
+    deleteSelf() {
+        CategoryList.deleteItem(this.id);
+    }
+
+    updateName(newName) {
+        this.name = newName;
+        // call the CategoryList updater interface to sync change
+    }
+}
+
+/**
  * A class for constructing Task Objects
  */
 class Task {
@@ -22,33 +43,27 @@ class Task {
     }
 
     deleteSelf() {
-        TaskList.deleteTask(this.id);
+        TaskList.deleteItem(this.id);
     }
 }
 
 /**
  * A singleton class providing public interfaces to work with the storage
  */
-class ListStorage {
-    // Name to give the  store in localStorage
-    static #key = 'task-list';
-
+class StoreManager {
     // Load the local data store
-    static loadStore() {
+    static loadStore(key) {
         try {
-            return JSON.parse(localStorage.getItem(this.#key));
+            return JSON.parse(localStorage.getItem(key));
         } catch (err) {
             console.error(err.message);
         }
     }
 
     // Update the local data store
-    static updateStore() {
+    static updateStore(key, func) {
         try {
-            localStorage.setItem(
-                this.#key,
-                JSON.stringify(TaskList.getTasks())
-            );
+            localStorage.setItem(key, JSON.stringify(func));
         } catch (err) {
             console.error(err.message);
         }
@@ -59,53 +74,115 @@ class ListStorage {
  * A singleton class providing public interfaces to work with the task list
  */
 class TaskList {
+    static #LIST_KEY = 'task-list-store';
     // Initialise taskList with localStorage
-    static #tasks;
+    static #items;
 
     // Private method to call if tasks is undefined to initialise from store
-    static #initialiseTasks() {
-        const store = ListStorage.loadStore();
+    static #initialise() {
+        const store = StoreManager.loadStore(this.#LIST_KEY);
 
         if (!store) {
-            this.#tasks = [];
+            this.#items = [];
         } else {
-            this.#tasks = store.map((item) => new Task(item));
+            this.#items = store.map((item) => new Task(item));
         }
     }
 
     // Public method to get tasks
-    static getTasks() {
-        if (!this.#tasks) this.#initialiseTasks();
+    static getItems() {
+        if (!this.#items) this.#initialise();
 
-        return this.#tasks;
+        return this.#items;
     }
 
     // Public method to add task
-    static addTask(task) {
-        if (!this.#tasks) this.#initialiseTasks();
+    static addItem(task) {
+        if (!this.#items) this.#initialise();
 
-        this.#tasks.push(task);
+        this.#items.push(task);
 
-        ListStorage.updateStore();
+        StoreManager.updateStore(this.#LIST_KEY, this.getTasks());
     }
 
     // Public method to delete task
-    static deleteTask(id) {
-        if (!this.#tasks) this.#initialiseTasks();
+    static deleteItem(id) {
+        if (!this.#items) this.#initialise();
 
-        if (this.#tasks.length === 0) return;
+        if (this.#items.length === 0) return;
 
-        const filteredTasks = this.#tasks.filter((task) => {
-            return task.id !== id;
+        const filteredItems = this.#items.filter((item) => {
+            return item.id !== id;
         });
 
-        this.#tasks = filteredTasks;
+        this.#items = filteredItems;
 
-        ListStorage.updateStore();
+        StoreManager.updateStore(this.#LIST_KEY, this.getItems());
     }
 
     // Public method to trigger updating the storage for changes to tasks
     static updateStore() {
-        ListStorage.updateStore();
+        StoreManager.updateStore(this.#LIST_KEY, this.getItems());
     }
 }
+
+/**
+ * A singleton class to manage the list of categories
+ */
+class CategoryList {
+    static #LIST_KEY = 'category-list-store';
+    static #items;
+
+    // Private method to call if categories list is undefined to initialise from store
+    static #initialise() {
+        const store = StoreManager.loadStore(this.#LIST_KEY);
+
+        if (!store) {
+            this.#items = [];
+        } else {
+            this.#items = store.map((item) => new Category(item));
+        }
+    }
+
+    // Public method to get tasks
+    static getItems() {
+        if (!this.#items) this.#initialise();
+
+        return this.#items;
+    }
+
+    // Public method to add task
+    static addItem(task) {
+        if (!this.#items) this.#initialise();
+
+        this.#items.push(task);
+
+        StoreManager.updateStore(this.#LIST_KEY, this.getItems());
+    }
+
+    // Public method to delete task
+    static deleteItem(id) {
+        if (!this.#items) this.#initialise();
+
+        if (this.#items.length === 0) return;
+
+        const filteredItems = this.#items.filter((item) => {
+            return item.id !== id;
+        });
+
+        this.#items = filteredItems;
+
+        StoreManager.updateStore(this.#LIST_KEY, this.getItems());
+    }
+
+    // Public method to trigger updating the storage for changes to tasks
+    static updateStore() {
+        StoreManager.updateStore(this.#LIST_KEY, this.getItems());
+    }
+}
+
+// CategoryList.addItem(new Category({ name: 'Category Name' }));
+console.log(TaskList.getItems());
+console.log(CategoryList.getItems());
+
+// CategoryList.getItems()[0].deleteSelf();

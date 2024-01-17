01c24be31a4fd303ddbaa3cf8c5ae9b51d4fa58e
Thomas McInnis <hey@thomasmcinnis.com>
2024-01-17 12:24:37 +1100

    Add simple task list render func

--- src/index.js
+++ src/index.js
@@ -1,139 +1,63 @@
 import './style.css';
 
-/**
- * A class for constructing Category Objects
- *  - Takes an object which must contain `name`
- */
-class Category {
-    constructor(formData) {
-        if (!formData || !formData.name)
-            throw new Error('Category name is required');
+import { taskList, categoryList, Task, Category } from './model';
 
-        const { id, name } = formData;
+console.log(taskList.getItems());
 
-        this.id = id === undefined ? self.crypto.randomUUID() : id;
-        this.name = name;
+function renderTask(index, item) {
+    const { name, date, category, isUrgent } = item;
+
+    // Create the container
+    const task = document.createElement('div');
+    task.setAttribute('data-id', index);
+    task.setAttribute('tabindex', '0');
+    if (isUrgent) task.setAttribute('urgent', 'true');
+
+    // Create the checkbox
+    const checkbox = document.createElement('input');
+    checkbox.setAttribute('type', 'checkbox');
+
+    task.appendChild(checkbox);
+
+    // Create the center content
+    const taskDetails = document.createElement('div');
+
+    const taskName = document.createElement('p');
+    taskName.textContent = name;
+    taskName.classList.add('task-name');
+    taskDetails.appendChild(taskName);
+
+    if (date) {
+        const taskDate = document.createElement('p');
+        taskDate.textContent = date;
+        taskDate.classList.add('task-date');
+        taskDetails.appendChild(taskDate);
     }
 
-    deleteSelf() {
-        CategoryList.deleteItem(this.id);
+    if (category) {
+        const taskCategory = document.createElement('p');
+        taskCategory.textContent = category;
+        taskCategory.classList.add('task-category');
+        taskDetails.appendChild(taskCategory);
     }
 
-    updateName(newName) {
-        this.name = newName;
-        CategoryList.updateStore();
-    }
+    task.appendChild(taskDetails);
+
+    // Create the urgent icon
+    const urgentButton = document.createElement('button');
+    urgentButton.classList.add('urgent-btn');
+
+    task.appendChild(urgentButton);
+
+    return task;
 }
 
-/**
- * A class for constructing Task Objects
- *  - Takes an object which must contain `name`
- *  - Optionally includes
- *      - category: id of category in Categories
- *      - dueDate: Date
- *      - isUrgent: Bool
- */
-class Task {
-    constructor(formData) {
-        if (!formData || !formData.name)
-            throw new Error('Task name is required');
+const tasksContainer = document.getElementById('tasks-list');
 
-        const { id, name, category, dueDate, isUrgent, createdDate } = formData;
+const tasks = taskList.getItems();
 
-        this.id = id === undefined ? self.crypto.randomUUID() : id;
-        this.name = name;
-        this.category = category;
-        this.dueDate = dueDate;
-        this.isUrgent = isUrgent === undefined ? false : isUrgent;
-        this.createdDate = createdDate === undefined ? new Date() : createdDate;
-    }
+tasks.forEach((item, index) => {
+    const task = renderTask(index, item);
 
-    toggleIsUrgent() {
-        this.isUrgent = !this.isUrgent;
-        TaskList.updateStore(); // don't love this ideally Task objects are unaware of updating the data store
-    }
-
-    deleteSelf() {
-        TaskList.deleteItem(this.id);
-    }
-}
-
-/**
- * A singleton class providing public interfaces to work with the storage
- */
-class StoreManager {
-    // Load the local data store
-    static loadStore(key) {
-        try {
-            return JSON.parse(localStorage.getItem(key));
-        } catch (err) {
-            console.error(err.message);
-        }
-    }
-
-    // Update the local data store
-    static updateStore(key, func) {
-        try {
-            localStorage.setItem(key, JSON.stringify(func));
-        } catch (err) {
-            console.error(err.message);
-        }
-    }
-}
-
-/**
- * A class for constructing List Objects
- */
-class ListManager {
-    constructor(listKey, ItemClass) {
-        this.LIST_KEY = listKey;
-        this.ItemClass = ItemClass;
-        this.items;
-    }
-
-    // Method to call if tasks is undefined to initialise from store
-    initialise() {
-        const store = StoreManager.loadStore(this.LIST_KEY);
-
-        this.items = store ? store.map((item) => new this.ItemClass(item)) : [];
-    }
-
-    // Method to get items list
-    getItems() {
-        if (!this.items) this.initialise();
-        return this.items;
-    }
-
-    // Method to add item to items list
-    addItem(item) {
-        if (!this.items) this.initialise();
-
-        this.items.push(new this.ItemClass(item));
-        this.updateStore();
-    }
-
-    // Method to delete item from items list
-    deleteItem(id) {
-        if (!this.items) this.initialise();
-
-        if (this.items.length === 0) return;
-
-        const filteredItems = this.items.filter((item) => {
-            return item.id !== id;
-        });
-
-        this.items = filteredItems;
-
-        this.updateStore();
-    }
-
-    // Method to trigger updating the storage from items list
-    updateStore() {
-        StoreManager.updateStore(this.LIST_KEY, this.getItems());
-    }
-}
-
-const TaskList = new ListManager('task-list-store', Task);
-const CategoryList = new ListManager('category-list-store', Category);
-
-console.log(TaskList.getItems());
+    tasksContainer.appendChild(task);
+});

8c243e5c0d8b41b8bfefc8f4c956d5b4f4f78818
Thomas McInnis <hey@thomasmcinnis.com>
2024-01-23 21:19:26 +1100

    Create dynamically populated form modal dialog element

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './style.css';
 import ListManager from './model/list-manager';
 import Task from './model/task';
 import renderTaskList from './components/tasks/tasklist';
+import { renderDialogForm } from './components/forms/forms';
 
 const taskList = new ListManager('task-list-store', Task);
 
@@ -43,5 +44,32 @@ function handleTaskListClick(event) {
     }
 }
 
-const tasksListElement = document.getElementById('tasks-list');
+const tasksListElement = document.querySelector('#tasks-list');
 tasksListElement.addEventListener('click', handleTaskListClick);
+
+//------------------------------------------------------------------------------
+const dialog = document.querySelector('dialog');
+const dialogCloseButton = document.querySelector('dialog button');
+
+// Get all the buttons on the page with class 'open-dialog'
+const openDialogButtons = document.querySelectorAll('button.open-dialog');
+
+// Add event listener for each, and selectively inject the right form
+openDialogButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        dialog.showModal();
+
+        const dialogForm = document.querySelector('dialog > form');
+
+        while (dialogForm.firstChild) {
+            dialogForm.removeChild(dialogForm.firstChild);
+        }
+
+        const form = renderDialogForm(button.dataset.action);
+        dialogForm.appendChild(form);
+    });
+});
+
+dialogCloseButton.addEventListener('click', () => {
+    dialog.close();
+});

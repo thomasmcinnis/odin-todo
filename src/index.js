9174dcce9c9b1859e1f2fe069a97e4bb6bc89f8b
Thomas McInnis <hey@thomasmcinnis.com>
2024-02-04 15:09:22 +1100

    Add dark and light styling

--- src/index.js
+++ src/index.js
@@ -11,11 +11,18 @@ categoryListManager.sub(renderCategoryList);
 categoryListManager.sub(updateTaskFormCategories);
 
 // Initialise lists, retrieving values from storage if they exist
+// This will also call subscribed view renderers with updated data
 categoryListManager.initList();
 taskListManager.initList();
 
 // DOM interaction stuff
+// - Add click handling for the main buttons to open their modals
+// - Add the modals and their interactivity
+// - Add click handling for the task list element
+// - Add click handling for the category list element
+
 mountModals();
+// TODO: mountModals should have all the forms split out, they shoud take the addItem() functions as callback functions rather than the listManagers being imported into those modules as it is right now. The forms should not know what is being done with their data on submit. Think this is called dependancy injection??
 
 function handleTaskListClick(event) {
     // only handle clicks on elements with an action attribute
@@ -52,6 +59,11 @@ function handleCategoryListClick(event) {
     // invoke relevant action with ID of item
     if (action === 'select') {
         categoryListManager.selectItem(categoryID);
+        // Curently the category list "knows" which category is selected
+        // Why? This should be the domain of the view only
+        // Create some view state object to track, let the view renderers sub
+        // to that instead therefore they can update without needing to cross-
+        // contaminate between data managers and view
     }
 }
 

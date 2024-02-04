d7f55cbfd46e0bb389d6e22e8e041d6af436331d
Thomas McInnis <hey@thomasmcinnis.com>
2024-02-04 20:47:12 +1100

    Add dummy data

--- src/index.js
+++ src/index.js
@@ -61,3 +61,41 @@ categoryListManager.sub(DisplayManager.updateView.bind(DisplayManager));
 //----- Initialise lists, retrieving values from storage
 categoryListManager.initList();
 taskListManager.initList();
+
+//----- Add demo categories and tasks if none exist
+const dummyCategories = [
+    {
+        name: 'Work',
+    },
+    {
+        name: 'Study',
+    },
+];
+
+const dummyTasks = [
+    {
+        name: 'File TPS report',
+        dueDate: '2024-01-21',
+        isUrgent: true,
+    },
+    {
+        name: 'Learn Javascript',
+    },
+];
+
+if (
+    taskListManager.getItems().length === 0 &&
+    categoryListManager.getItems().length === 0
+) {
+    dummyCategories.forEach((category) =>
+        categoryListManager.addItem(category)
+    );
+
+    const categories = categoryListManager.getItems();
+
+    dummyTasks.forEach((task, index) => {
+        const categoryID = categories[index].id;
+        task.categoryID = categoryID;
+        taskListManager.addItem(task);
+    });
+}
